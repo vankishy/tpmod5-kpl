5430409ac73c5019daefe3699287ca84be1febf9
Reinhard Situmeang <101586289+vankishy@users.noreply.github.com>
2024-03-16 21:01:34 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<string>("Reinhard");
+    }
+}
+
+class HaloGeneric
+{
+    public static void SapaUser<T>(T X)
+    {
+        Console.WriteLine($"Halo user {X}", X);
+    }
+}
